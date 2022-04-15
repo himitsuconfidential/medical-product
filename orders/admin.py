a4bd5f37da51cd533041c9148bc6bfdbb0a72a3a
himitsuconfidential <aperson328@gmail.com>
2022-04-15 18:00:14 +0800

    build database model and admin (up to tutorial 2 page24)

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(ClinicManager)
+admin.site.register(MedicalProduct)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class ClinicManager(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+class MedicalProduct(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    time = models.DateTimeField()
+    status = models.CharField(max_length=10)
+    manager = models.ForeignKey(ClinicManager, on_delete=models.CASCADE)
+    products = models.ManyToManyField(MedicalProduct, through='OrderItem')
+    def __str__(self):
+        return f'{self.manager.name} ({self.time})'
+    
+class OrderItem(models.Model):
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(MedicalProduct, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.product.name} x({self.quantity})'
